51a7a1209947169f9fbb515d5a3ccfdf868ae584
Tofik Khan <tofik.khan@mkausa.org>
2025-05-13 18:18:09 -0700

    Add QueryClient Provider to main file

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,18 @@ import { createRoot } from "react-dom/client";
 import App from './App'
 import { AuthProvider } from "./AuthProvider.tsx";
 import { BrowserRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-    <AuthProvider>
-            <App />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 );
