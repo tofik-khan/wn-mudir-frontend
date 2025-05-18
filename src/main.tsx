22aa55c1ee1045d7136d083fec1dc93cd38b0a75
Tofik Khan <tofik.khan@mkausa.org>
2025-05-18 11:08:03 -0700

    Add redux provider to main application

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from './App'
+import App from "./App";
 import { AuthProvider } from "./AuthProvider.tsx";
 import { BrowserRouter } from "react-router";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "@/theme/mainTheme.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { LicenseInfo } from "@mui/x-license";
+import store from "./store.ts";
+import { Provider } from "react-redux";
 
 LicenseInfo.setLicenseKey(import.meta.env.VITE_MUI_X_PRO_LICENSE);
 
@@ -15,13 +17,15 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <ThemeProvider theme={theme}>
-            <App />
-          </ThemeProvider>
-        </AuthProvider>
-      </QueryClientProvider>
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <ThemeProvider theme={theme}>
+              <App />
+            </ThemeProvider>
+          </AuthProvider>
+        </QueryClientProvider>
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );
