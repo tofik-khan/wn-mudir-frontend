2b2e5f75879f318504848ac45f1f4d18a0b0c6d7
Tofik Khan <tofik.khan@mkausa.org>
2025-06-20 01:37:19 -0700

    Create Dashboard cards

---
+++ src/components/waqfeardhi/ApplicationsCountCard.tsx
@@ -0,0 +1,39 @@
+import { useApplicationsCountQuery } from "@/queries/waqfeardhi/dashboard";
+import { Box, CircularProgress, Paper, Typography } from "@mui/material";
+
+export const ApplicationsCountCard = () => {
+  const { data: count, isLoading } = useApplicationsCountQuery();
+
+  return (
+    <>
+      <Paper
+        variant="outlined"
+        sx={{
+          padding: 2,
+          height: "100px",
+          mr: 2,
+        }}
+      >
+        {isLoading ? (
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100%",
+            }}
+          >
+            <CircularProgress />
+          </Box>
+        ) : (
+          <>
+            <Typography variant="overline">Total Applications</Typography>
+            <Typography variant="h4" fontWeight={"bold"}>
+              {count}
+            </Typography>
+          </>
+        )}
+      </Paper>
+    </>
+  );
+};
