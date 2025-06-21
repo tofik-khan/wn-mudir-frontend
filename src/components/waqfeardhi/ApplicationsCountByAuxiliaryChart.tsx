c0ee73843c1e2ae84d48f434bc0b9e87fdd38508
Tofik Khan <tofik.khan@mkausa.org>
2025-06-20 23:36:25 -0700

    Create Chart component

---
+++ src/components/waqfeardhi/ApplicationsCountByAuxiliaryChart.tsx
@@ -0,0 +1,26 @@
+import { useApplicationsCountByAuxiliaryQuery } from "@/queries/waqfeardhi/dashboard";
+import { Box, CircularProgress } from "@mui/material";
+import { PieChart } from "@mui/x-charts/PieChart";
+
+export const ApplicationsCountByAuxiliaryChart = () => {
+  const { data, isLoading } = useApplicationsCountByAuxiliaryQuery();
+
+  if (isLoading || !data) {
+    return (
+      <>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100%",
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      </>
+    );
+  }
+
+  return <PieChart series={[{ data, innerRadius: 75 }]} />;
+};
