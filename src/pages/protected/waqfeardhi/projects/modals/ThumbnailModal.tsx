c35f5d5696b77862e3de1647dd03d59bdfcde21d
Tofik Khan <tofik.khan@mkausa.org>
2026-02-07 12:59:52 -0800

    Update Projects modals to get common gallery component

--- src/pages/protected/waqfeardhi/projects/modals/ThumbnailModal.tsx
+++ src/pages/protected/waqfeardhi/projects/modals/ThumbnailModal.tsx
@@ -3,70 +3,13 @@ import {
   DialogTitle,
   DialogContent,
   IconButton,
-  Typography,
   Box,
   CircularProgress,
-  ImageList,
-  ImageListItem,
-  ImageListItemBar,
 } from "@mui/material";
-import { Close, HideImage } from "@mui/icons-material";
+import { Close } from "@mui/icons-material";
 import { useImageKitGetFolderAssets } from "@/queries/mudir/images";
 import { waqfeArdhiFolders } from "@/constants";
-
-const Gallery = ({ data, onClick, onClose }) => {
-  if (data.length < 1) {
-    return (
-      <>
-        <HideImage
-          sx={(theme) => ({
-            width: "100px",
-            height: "100px",
-            color: theme.palette.action.focus,
-            my: 2,
-          })}
-        />
-        <Typography variant="h3">No Images Available</Typography>
-      </>
-    );
-  }
-
-  return (
-    <>
-      <Box sx={{ margin: "auto" }}>
-        <ImageList sx={{ width: "100%" }} cols={4}>
-          {data.map((item) => (
-            <ImageListItem
-              key={item.fileId}
-              sx={(theme) => ({
-                width: "200px",
-                height: "150px",
-                my: 2,
-                border: `1px solid ${theme.palette.grey[300]}`,
-                borderRadius: "4px",
-              })}
-              onClick={() => {
-                onClick(item.url);
-                onClose();
-              }}
-            >
-              <img src={`${item.thumbnail}`} alt={item.name} loading="lazy" />
-              <ImageListItemBar
-                position="below"
-                sx={(theme) => ({
-                  background: theme.palette.grey[400] + "44",
-                  padding: 1,
-                  mt: 1,
-                })}
-                title={item.name}
-              />
-            </ImageListItem>
-          ))}
-        </ImageList>
-      </Box>
-    </>
-  );
-};
+import { Gallery } from "@/components/Gallery";
 
 export const ThumbnailModals = ({
   open,
