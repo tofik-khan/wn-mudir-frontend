a84d0b81dc97812d0e4507ed9e6a877cdb714ad9
Tofik Khan <tofik.khan@mkausa.org>
2026-02-07 12:55:25 -0800

    Create Modal to select Presenter Image

---
+++ src/pages/protected/expo/presenters/modals/selectImageModal.tsx
@@ -0,0 +1,69 @@
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  IconButton,
+  Box,
+  CircularProgress,
+} from "@mui/material";
+import { Close } from "@mui/icons-material";
+import { useImageKitGetFolderAssets } from "@/queries/mudir/images";
+import { expoFolders } from "@/constants";
+import { Gallery } from "@/components/Gallery";
+
+export const PresenterImageModal = ({
+  open,
+  onClose,
+  onClick,
+}: {
+  open: boolean;
+  onClose: any;
+  onClick: any;
+}) => {
+  const folder = expoFolders[0];
+  const { data, isLoading } = useImageKitGetFolderAssets({
+    folder,
+  });
+  return (
+    <>
+      <Dialog open={open} onClose={onClose} fullWidth maxWidth={"lg"}>
+        <DialogTitle>Select Presenter Image</DialogTitle>
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={(theme) => ({
+            position: "absolute",
+            right: 8,
+            top: 8,
+            color: theme.palette.grey[500],
+          })}
+        >
+          <Close />
+        </IconButton>
+        <DialogContent
+          dividers
+          sx={{
+            pl: "16px",
+            display: "flex",
+            flexDirection: "column",
+            gap: "24px",
+          }}
+        >
+          {isLoading ? (
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <CircularProgress />
+            </Box>
+          ) : (
+            <Gallery data={data} onClick={onClick} onClose={onClose} />
+          )}
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
